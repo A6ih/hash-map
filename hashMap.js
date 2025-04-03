e93d5cb75f5775fda00721306592f8ffd1ff1a29
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-04 01:17:15 +0300

    Add method to grow the buckets array when no of entries exceeds the product of capacity and load factor, Change logic for clear() method, it will reset the capacity to default of 16

--- hashMap.js
+++ hashMap.js
@@ -1,8 +1,11 @@
 import LinkedList from "./linkedList.js";
 
 class HashMap {
-  constructor() {
-    (this.buckets = []), (this.capacity = 16), (this.loadFactor = 0.8);
+  constructor(loadFactor) {
+    (this.buckets = []),
+      (this.capacity = 16),
+      (this.loadFactor = loadFactor),
+      (this.buckets.length = this.capacity);
   }
 
   hash(key) {
@@ -20,18 +23,25 @@ class HashMap {
   set(key, value) {
     const hashCode = this.hash(key);
 
+    if (hashCode < 0 || hashCode >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
     if (!this.buckets[hashCode]) {
       this.buckets[hashCode] = new LinkedList();
       this.buckets[hashCode].prepend(key, value);
+      this.growBuckets();
       return;
     }
 
     if (this.buckets[hashCode].size === 1) {
       if (this.buckets[hashCode].head.key === key) {
         this.buckets[hashCode].head.value = value;
+        this.growBuckets();
         return;
       }
       this.buckets[hashCode].prepend(key, value);
+      this.growBuckets();
       return;
     }
 
@@ -39,10 +49,11 @@ class HashMap {
       const index = this.buckets[hashCode].find(key);
       const node = this.buckets[hashCode].at(index);
       node.value = value;
+      this.growBuckets();
       return;
     } else {
       this.buckets[hashCode].prepend(key, value);
-      return;
+      this.growBuckets();
     }
   }
 
@@ -134,6 +145,8 @@ class HashMap {
 
   clear() {
     this.buckets.length = 0;
+    this.capacity = 16;
+    this.buckets.length = this.capacity;
   }
 
   keys() {
@@ -210,9 +223,44 @@ class HashMap {
 
     return arr;
   }
+
+  entries() {
+    const arr = [];
+
+    if (this.buckets.length === 0) {
+      return arr;
+    }
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i]) {
+        if (this.buckets[i].size === 1) {
+          arr.push([this.buckets[i].head.key, this.buckets[i].head.value]);
+        } else {
+          let current = this.buckets[i].head;
+
+          while (current) {
+            arr.push([current.key, current.value]);
+            current = current.nextNode;
+          }
+        }
+      }
+    }
+
+    return arr;
+  }
+
+  growBuckets() {
+    if (this.buckets.length * this.loadFactor < this.length()) {
+      const tempArr = this.entries();
+      this.buckets.length = 0;
+      this.capacity = this.capacity * 2;
+      this.buckets.length = this.capacity;
+      tempArr.forEach(arr => this.set(arr[0], arr[1]));
+    }
+  }
 }
 
-const test = new HashMap();
+const test = new HashMap(0.75);
 
 test.set("apple", "red");
 test.set("banana", "yellow");
@@ -226,6 +274,29 @@ test.set("ice cream", "white");
 test.set("jacket", "blue");
 test.set("kite", "pink");
 test.set("lion", "golden");
+test.set("moon", "silver");
+test.set("apple", "redish");
+test.set("jacket", "blueish");
+test.set("lion", "yelloish");
+test.set("grape", "violet");
+test.clear();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+test.set("moon", "silver");
+test.set("apple", "redish");
+test.set("jacket", "blueish");
+test.set("lion", "yelloish");
+test.set("grape", "violet");
 
 console.log(test.buckets);
 console.log(test.buckets.length);
@@ -234,6 +305,6 @@ console.log(test.get("ice cream"));
 console.log(test.has("ABhi"));
 console.log(test.has("carrot"));
 console.log(test.length());
-console.log(test.length());
 console.log(test.keys());
 console.log(test.values());
+console.log(test.entries());
